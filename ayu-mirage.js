156a2257225578e422d4ee81ead9cfa10a8adbfd
Ian Grant <ian@chenghiz.net>
2018-04-23 13:33:59 -0700

    woops, got those colors backwards!

--- ayu-mirage.js
+++ ayu-mirage.js
@@ -3,22 +3,22 @@ exports.decorateConfig = config => {
         foregroundColor: '#d9d7ce',
         backgroundColor: '#212733',
         colors: {
-            black: '#343d4a',
+            black: '#686868',
             red: '#f07178',
-            green: '#bae67e',
-            yellow: '#ffcc66',
-            blue: '#36a3d9',
-            magenta: '#d4bfff',
-            cyan: '#95e6cb',
-            white: '#c7c7c7',
-            lightBlack: '#686868',
+            green: '#cbe645',
+            yellow: '#ffdf80',
+            blue: '#6871ff',
+            magenta: '#ff77ff',
+            cyan: '#a6fde1',
+            white: '#ffffff',
+            lightBlack: '#343d4a',
             lightRed: '#f07178',
-            lightGreen: '#cbe645',
-            lightYellow: '#ffdf80',
-            lightBlue: '#6871ff',
-            lightMagenta: '#ff77ff',
-            lightCyan: '#a6fde1',
-            lightWhite: '#ffffff'
+            lightGreen: '#bae67e',
+            lightYellow: '#ffcc66',
+            lightBlue: '#36a3d9',
+            lightMagenta: '#d4bfff',
+            lightCyan: '#95e6cb',
+            lightWhite: '#c7c7c7'
         }
     });
 };
